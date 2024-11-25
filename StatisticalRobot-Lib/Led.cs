4cf98cc999447113969ecfa62c6bfcfe39b66ddf
Arczius <arczius.development@gmail.com>
2024-11-25 15:18:29 +0100

    added some functionality to check if the light is turned on or off

--- StatisticalRobot-Lib/Led.cs
+++ StatisticalRobot-Lib/Led.cs
@@ -5,6 +5,7 @@ namespace Avans.StatisticalRobot;
 public class Led
 {
     private readonly int _pin;
+    private bool _status;
 
     /// <summary>
     /// This is a digital device
@@ -15,14 +16,22 @@ public class Led
     {
         Robot.SetDigitalPinMode(pin, PinMode.Output);
         _pin = pin;
+        _status = false;
     }
 
     public void SetOn()
     {
         Robot.WriteDigitalPin(_pin, PinValue.High);
+        _status = true;
     }
     public void SetOff()
     {
         Robot.WriteDigitalPin(_pin, PinValue.Low);
+        _status = false;
+    }
+
+    public string GetStatus()
+    {
+        return _status ? "on" : "off";
     }
 }
